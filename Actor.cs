af1acfe7c4c40f949dcdd4467fa9aaf35ceca4e3
Alister Brumley <109202180+AlisterBrumley@users.noreply.github.com>
2024-09-13 18:29:15 +1000

    added graphics and new enemy types

--- Actor.cs
+++ Actor.cs
@@ -8,7 +8,8 @@ public class Actor
     public int RangedAtk; // range attack base damage
     public int Speed; // evasion base stat
 
-    public string actionState = "pass";
+    public string? actionState;
+    public string Graphic = "       \n       "; // empty space by default
 
     public Actor(string name, int hp, int def, int melee, int ranged, int speed)
     {
@@ -19,6 +20,17 @@ public class Actor
         MeleeAtk = melee;
         RangedAtk = ranged;
         Speed = speed;
+    }
+    public Actor(string name, int hp, int def, int melee, int ranged, int speed, string graphic)
+    {
+
+        Name = name;
+        Health = hp;
+        Def = def;
+        MeleeAtk = melee;
+        RangedAtk = ranged;
+        Speed = speed;
+        Graphic = graphic;
     }
 
     public void Damaged(int baseDmg, int roll)
@@ -55,18 +67,27 @@ public class Player : Actor
 
 public class Ogre : Actor
 {
-    public Ogre() : base(name: "Ogre", hp: 100, def: 30, melee: 30, ranged: 0, speed: 20) { }
+    public Ogre() : base(name: "Ogre", hp: 100, def: 30, melee: 30, ranged: 0, speed: 20, graphic: ",|,,,|,\n(o _ o)") { }
     // public Ogre(string customName) : base(name: customName, hp: 100, def: 30, melee: 30, ranged: 0, speed: 20) { }
-    public Ogre(string customName, int customHp) : base(name: customName, hp: customHp, def: 30, melee: 30, ranged: 0, speed: 35) { }
+    public Ogre(string customName, int customHp) : base(name: customName, hp: customHp, def: 30, melee: 30, ranged: 0, speed: 35, graphic: "O>   <O\n(T _ T)") { }
 }
 
 public class Slime : Actor
 {
-    public Slime() : base(name: "Slime", hp: 30, def: 10, melee: 10, ranged: 10, speed: 10) { }
+    public Slime() : base(name: "Slime", hp: 30, def: 10, melee: 10, ranged: 10, speed: 10, graphic: "       \n(o - O)") { }
 }
 
 public class CyberDemon : Actor
 {
-    public CyberDemon() : base(name: "CyberDemon", hp: 500, def: 200, melee: 120, ranged: 200, speed: 20) { }
+    public CyberDemon() : base(name: "CyberDemon", hp: 500, def: 200, melee: 120, ranged: 200, speed: 20, graphic: " }{ }{ \n(Ø>_<Ø)") { }
 }
 
+public class Bug : Actor
+{
+    public Bug() : base(name: "Bug", hp: 15, def: 5, melee: 10, ranged: 10, speed: 10, graphic: "°\\, ,/°\n(# m #)") { }
+}
+
+public class Bunny : Actor
+{
+    public Bunny() : base(name: "Bunny", hp: 5, def: 0, melee: 1, ranged: 0, speed: 1, graphic: " /\\ /\\ \n(o x o)") { }
+}
