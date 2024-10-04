9186fd92b59c188bc23e5d4887ff455407fc755a
Alister Brumley <109202180+AlisterBrumley@users.noreply.github.com>
2024-10-04 15:16:02 +1000

    updated melee attack method

--- Actor.cs
+++ Actor.cs
@@ -3,7 +3,7 @@ public class Actor
 {
     public string Name;
     public int Health;
-    public int Def; // defense multiplier base stat
+    public int Def; // adjustDefense multiplier base stat
     public int MeleeAtk; // melee attack base damage
     public int RangedAtk; // range attack base damage
     public int Speed; // evasion base stat
@@ -34,40 +34,53 @@ public class Actor
         Graphic = graphic;
     }
 
-    // Might be better to make this an attack rather then damage
-    // public void Damaged(int baseDmg, int roll)
-    // {
-    //     int damage = baseDmg + (Def - roll);
-    //     int crit = 2 * (Health / 3);
-
-    //     if (damage >= crit)
-    //     {
-    //         Console.WriteLine("critical hit!");
-    //         Console.WriteLine($"hit {Name} with {damage} damage!");
-    //     }
-    //     else
-    //     {
-    //         Console.WriteLine($"hit {Name} with {damage} damage!");
-    //     }
-    //     Console.WriteLine();
-    // }
-
     public bool Melee(Actor target)
     {
-        // rolling for damage
-        int dmgRoll = Game.Roll();
-        int damage = (int)(MeleeAtk * (dmgRoll * 5 / 100m));
-        // Writer.DebugWrite("ATK:" + damage, 2000);
-
-        // rolling for defense
-        int defRoll = Game.Roll();
-        int defense = (int)(target.Def * (defRoll * 5 / 100m));
-        // Writer.DebugWrite("DEF:" + defense, 2000);
-
-        // Writer.DebugWrite(target.Name + " HP:" + target.Health + " DMG:" + damage + " DEF:" + defense);
-
         // rolling for evasion
-        //      // MISSES TOO MUCH?
+        int dodgeRoll = Game.Roll();
+        int hitRoll = Game.Roll();
+        if (hitRoll < 5)
+        {
+            Writer.DebugWrite("HIT " + hitRoll);
+            Writer.MessageWrite(this.Name + " missed!");
+            return false;
+        }
+        else if (dodgeRoll > 18)
+        {
+            Writer.DebugWrite("DOD " + dodgeRoll);
+            Writer.MessageWrite(target.Name + " dodged the attack!");
+            return true;
+        }
+        else if (target.GetType() == typeof(Player) && dodgeRoll > 10)
+        {
+            Writer.DebugWrite("DOD " + dodgeRoll);
+            Writer.MessageWrite(target.Name + " dodged the attack!!");
+            return true;
+        }
+
+        // random damage adjustment
+        //      // TOFIX - tends to get low damage output on eneies
+        int dmgRoll = Game.Roll();
+        int adjustDamage = (int)(MeleeAtk / 4 * (dmgRoll * 5 / 100m));
+        // random defense adjustment
+        int defRoll = Game.Roll();
+        int adjustDefense = (int)(target.Def * (defRoll * 5 / 100m) / 2);
+
+
+        int damage = MeleeAtk + adjustDamage - adjustDefense;
+        // taking care of overrun 
+        damage = damage <= 0 ? 1 : damage;
+        target.Health -= damage;
+
+        Writer.DebugWrite(target.Name + " HP:" + target.Health + " DMG:" + damage + " DEF:" + adjustDefense + " ADJ:" + adjustDamage);
+
+        return true;
+    }
+
+    public bool Ranged(Actor target)
+    {
+        // rolling for evasion
+        //      // TOFIX - enemies miss too much
         int dodgeRoll = Game.Roll();
         int dodgeSpeed = (int)(target.Speed * (dodgeRoll * 5 / 100m));
         int hitRoll = Game.Roll();
@@ -78,39 +91,13 @@ public class Actor
             return false;
         }
 
-        // taking care of overrun
-        damage = defense >= damage ? 1 : damage;
-        target.Health -= damage;
-        Writer.DebugWrite(target.Health);
-
-        // TODO - FIGURE THIS OUT?
-        // int damage = MeleeAtk + (target.Def - roll);
-        // int crit = 2 * (Health / 3);
-
-        // int crit = target.Def % roll;
-        // if (crit > 10)
-        // {
-        //     target.Health = target.Health - damage;
-        //     // Console.WriteLine("critical hit!");
-        //     // Console.WriteLine($"hit {Name} with {damage} damage!");
-        // }
-        // else
-        // {
-        //     // Console.WriteLine($"hit {Name} with {damage} damage!");
-        // }
-
         return true;
-    }
-
-    public void Ranged(Actor target, int roll)
-    {
-
     }
 }
 
 public class Player : Actor
 {
-    public Player() : base(name: "Player", hp: 150, def: 120, melee: 50, ranged: 50, speed: 30) { }
+    public Player() : base(name: "Player", hp: 150, def: 80, melee: 50, ranged: 50, speed: 35) { }
     public Player(string customName) : base(name: customName, hp: 150, def: 120, melee: 50, ranged: 50, speed: 30) { }
 
     // add debug/cheat mode
@@ -119,27 +106,26 @@ public class Player : Actor
 
 public class Ogre : Actor
 {
-    public Ogre() : base(name: "Ogre", hp: 100, def: 30, melee: 30, ranged: 0, speed: 20, graphic: ",|,,,|,\n(o _ o)") { }
-    // public Ogre(string customName) : base(name: customName, hp: 100, def: 30, melee: 30, ranged: 0, speed: 20) { }
+    public Ogre() : base(name: "Ogre", hp: 100, def: 30, melee: 25, ranged: 0, speed: 20, graphic: ",|,,,|,\n(o _ o)") { }
     public Ogre(string customName, int customHp) : base(name: customName, hp: customHp, def: 30, melee: 30, ranged: 0, speed: 35, graphic: "O>   <O\n(T _ T)") { }
 }
 
 public class Slime : Actor
 {
-    public Slime() : base(name: "Slime", hp: 30, def: 10, melee: 10, ranged: 10, speed: 10, graphic: "       \n(o - O)") { }
+    public Slime() : base(name: "Slime", hp: 30, def: 10, melee: 15, ranged: 10, speed: 25, graphic: "       \n(o - O)") { }
 }
 
 public class CyberDemon : Actor
 {
-    public CyberDemon() : base(name: "CyberDemon", hp: 500, def: 200, melee: 120, ranged: 200, speed: 20, graphic: " }{ }{ \n(Ø>_<Ø)") { }
+    public CyberDemon() : base(name: "CyberDemon", hp: 500, def: 200, melee: 120, ranged: 200, speed: 10, graphic: " }{ }{ \n(Ø>_<Ø)") { }
 }
 
 public class Bug : Actor
 {
-    public Bug() : base(name: "Bug", hp: 15, def: 5, melee: 10, ranged: 10, speed: 10, graphic: "°\\, ,/°\n(# m #)") { }
+    public Bug() : base(name: "Bug", hp: 15, def: 5, melee: 10, ranged: 10, speed: 35, graphic: "°\\, ,/°\n(# m #)") { }
 }
 
 public class Bunny : Actor
 {
-    public Bunny() : base(name: "Bunny", hp: 5, def: 0, melee: 1, ranged: 0, speed: 1, graphic: " /\\ /\\ \n(o x o)") { }
+    public Bunny() : base(name: "Bunny", hp: 5, def: 0, melee: 1, ranged: 0, speed: 100, graphic: " /\\ /\\ \n(o x o)") { }
 }
